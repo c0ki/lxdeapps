fdae92a6b853190dd1307faa5f2321e888bd6552
c0ki <cokinou.web@gmail.com>
2018-03-06 16:31:30 +0100

    Adjust for last string ending by ':'

--- fconvert/filters/pandoc-list.py
+++ fconvert/filters/pandoc-list.py
@@ -13,22 +13,21 @@ def filterList(key, value, fmt, meta):
     if fmt == 'beamer':
         fmt = 'latex'
     if fmt == 'latex':
-        #latex_begin = r'\vspace{-\the\parskip}\begin{itemize}\setlength{\parskip}{1em}\vspace{-\the\parskip}\item[]'
         latex_begin = r'\vspace{-\the\parskip}'
-        #latex_end = r'\end{itemize}'
         latex_end = r''
         global lastStr
         try:
             lastStr
         except NameError:
             lastStr = None
+        
         if key == 'Str':
             #sys.stderr.write("value %s\n" % value.encode('utf-8'))
             lastStr = value
-        elif (key == 'OrderedList' and lastStr == ':'):
+        elif (key == 'OrderedList' and lastStr[-1] == ':'):
             attrs, items = value
             return [RawBlock(fmt, latex_begin)] + [OrderedList(attrs, items)] + [RawBlock(fmt, latex_end)]
-        elif (key == 'BulletList' and lastStr == ':'):
+        elif (key == 'BulletList' and lastStr[-1] == ':'):
             items = value
             return [RawBlock(fmt, latex_begin)] + [BulletList(items)] + [RawBlock(fmt, latex_end)]
 
